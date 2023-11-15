218353f90ac0b45455b1824db5fd1acada0aeefe
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-11-15 19:54:52 +0800

    innit push

---
+++ src/data/socials.ts
@@ -0,0 +1,11 @@
+export const author = "Space Faculty Asia";
+
+export const defaultMeta = {
+  title: "Space Faculty Asia",
+  description:
+    "Creating Opportunities for Students to Learn and Grow in the Space Industry",
+  ogImage: {
+    src: "/static/banner.png",
+    alt: "Space Faculty Asia",
+  },
+};
